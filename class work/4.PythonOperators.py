de75afaf3ac1ebe1b34ed9f6b28ee24b2f6f98d7
Latha539 <hemalathapogaku539@gmail.com>
2025-07-02 12:35:59 +0530

    add all

---
+++ class work/4.PythonOperators.py
@@ -0,0 +1,19 @@
+a=40
+b=50
+print("Addition(+):",a+b)
+print("Subtraction(-):",a-b)
+print("Multiplication(*):",a*b)
+print("Division(/):",a/b)
+print("Floor Division(//):",a//b)
+print("Modulus(%):",a%b)
+print("exponentional(**):",a**b)
+
+#2.comparison operators
+print("equal to(==):",a==b)
+print("Not Equal to(!=):",a!=b)
+print("Greater than(>):",a>b)
+print("Less than(<):",a<b)
+print("Greater than or Equal to(>=):",a>=b)                
+print("lesser than or equal to(<=):",a<=b)
+
+#3.Assignment Operators
