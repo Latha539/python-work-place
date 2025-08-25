c2711dc67338f773cc4ba588e0cf53c44a4c751f
Latha539 <hemalathapogaku539@gmail.com>
2025-08-25 12:05:20 +0530

    check1

--- praticework/pratice_py1.py
+++ praticework/pratice_py1.py
@@ -1,1 +1,14 @@
-print("classworks")
+ # iinput statements:
+
+
+a = int(input("Give a value:"))
+b = int(input("give b value:"))
+
+print(a,b sep = "Hello world")
+
+
+
+# name = input()
+# age = int(input())
+
+# print("Hello, my name is: ",name,age,name )
